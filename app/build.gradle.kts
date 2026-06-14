4abc5eff0d2e1ad9e25dd2ef69525968f051bac3
Pombo <38018741+pomboverso@users.noreply.github.com>
2026-06-14 14:51:11 -0700

    fixing skipping rest timer (#21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.rama.txori"
         minSdk = 21
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "$currentYear.$versionCode"
     }
 

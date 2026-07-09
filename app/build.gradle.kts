84eefec3c9e7551735f05d429fd983ec229ca623
Pombo <github.5yeil@simplelogin.fr>
2026-07-08 22:30:57 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.rama.txori"
         minSdk = 21
         targetSdk = 37
-        versionCode = 46
+        versionCode = 13
         versionName = "$currentYear.$versionCode"
     }
 
